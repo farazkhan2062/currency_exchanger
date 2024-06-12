54a840853c583c3eb53622d32949266353176c51
farazkhan2062 <farazkhan2062@gmail.com>
2024-06-12 10:30:55 +0500

    "Add Chalk"

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 #! /usr/bin/env node
 
+// The TypeScript console app is used to convert currencies: the users enter a certain amount of money in one 
+//currency and set the currency they want to check the monetary value of.
+// While developing the app, the beginners can master variables, algorithms, loops, if statements, and other 
+//TypeScript concepts.
+// Create a GitHub repository for the project and submit its URL in the project submission form.
+
+
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 const currency: any = {
   USD: 1,
@@ -15,25 +23,25 @@ let userAnswer = await inquirer.prompt([
     name: "from",
     type: "list",
     choices: ["USD", "PKR", "INR", "EUR", "SAR"],
-    message: "Select your currency from",
+    message: chalk.blue("Select your currency from"),
   },
   {
     name: "to",
     type: "list",
     choices: ["USD", "PKR", "INR", "EUR", "SAR"],
-    message: "Select your currency to",
+    message: chalk.green("Select your currency to"),
   },
   {
     name: "amount",
     type: "number",
-    message: "Enter your amount",
+    message: chalk.yellow("Enter your amount"),
   },
 ]);
 
 let fromAnswer = currency[userAnswer.from];
 let toAnswer = currency[userAnswer.to];
-let amountAnswer = userAnswer.amount;
+let amountAnswer = (userAnswer.amount);
 let baseAmount = amountAnswer / fromAnswer;
 let result: number = baseAmount * toAnswer;
-let result1 = result.toFixed(3);
-console.log(result1);
+let result1 = result.toFixed(2);
+console.log(chalk.magentaBright.bold.italic(`\nyour amount is ${result1}`));
